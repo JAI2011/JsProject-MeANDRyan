176fdcf2f582867831c5f3c571686683f5184c44
JAI-THE-GUY <jai.mokashi@gmail.com>
2025-03-31 18:55:24 -0500

    Made the backgound color change when enter is pressed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+
+
+
+document.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+        console.log("Enter key pressed");
+        document.body.style.backgroundColor = (Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256));
+    }
+});
